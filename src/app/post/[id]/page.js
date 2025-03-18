8d7c184b7853380ce79ac6c1a25c6f3a6fe76ca2
ambika-encipherhealth <ambika@encipherhealth.com>
2025-03-18 15:52:58 +0530

    chnages

--- src/app/post/[id]/page.js
+++ src/app/post/[id]/page.js
@@ -1,5 +1,5 @@
 async function getData(id) {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id+1}`);
     if (!res.ok) throw new Error("Failed to fetch data");
     return res.json();
   }
